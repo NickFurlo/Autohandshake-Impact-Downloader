8d6cc83290ea6cff6ae4ec9ce67a15543463a054
NickFurlo <NickBFurlo@Gmail.com>
2019-12-09 16:19:39 -0500

    Re-wrote event page, it no longer requires a saved search or a click from the user. Added Comments. Fixed deletion of old files. Updated config file.

--- ImpactDownloader.py
+++ ImpactDownloader.py
@@ -1,4 +1,4 @@
-# Impact Downloader script by Nick Furlo 5/21/18
+# Impact Downloader script by Nick Furlo 2018-2019
 import configparser
 import csv
 import glob
@@ -6,18 +6,17 @@ import itertools
 import os
 import shutil
 import time
+import autohandshake
 from datetime import datetime
 from pathlib import Path
 from tkinter import *
 from tkinter import messagebox
-from autohandshake import HandshakeSession, InsightsPage, SurveyPage, EventsPage
+from autohandshake import InsightsPage, SurveyPage, EventsPage
 from tqdm import tqdm
-import autohandshake
 
-print("first")
 
 class ImpactDownloader:
-
+    # loads CSV file into urls dictionary
     def load_csv(self):
         with open(input_file_path, mode='r') as infile:
             urls = {rows[0]: rows[1] for rows in itertools.islice(csv.reader(infile), 1, number_of_rows)}
@@ -30,7 +29,7 @@ class ImpactDownloader:
         print("Checking for files older than " + str(cutoff) + " days.")
         deleted = 0
         for path in network_paths:
-
+            # load all fiels objects into "files"
             files = os.listdir(path)
             for xfile in files:
                 full_pth = Path(os.path.join(path, xfile))
@@ -69,7 +68,7 @@ class ImpactDownloader:
         except Exception as e:
             print("Error Deleting Old Files: " + str(e))
 
-    # Copies files into folder with names the same as the name of the file without datetime. These folders are in a
+    # Copies files into folder with the name located in column A. These folders are in a
     # network location specified by the user.
     def copy_to_network_drive(self):
         copied = 0
@@ -80,36 +79,41 @@ class ImpactDownloader:
         except:
             print("Could not get files to move to network location.")
         try:
-            print("Enter try catch")
             print("files: " + str(files))
             for file in files:
-                print("enter for loop")
+                # fixes path text
                 full_path = os.path.join(network_location, file[:-15])
                 full_path = full_path.replace("\\", "/")
                 print(full_path)
                 network_paths.append(full_path)
-                print("Start Shutil")
+                # use shutil to copy file to folder
                 shutil.copy2(file, full_path)
-                print("Copied File To: " + full_path)
+                print("Copied " + str(file) + " to " + full_path)
                 copied += 1
-            print("End of for loop")
         except Exception as e:
             print("Could not copy files to network location")
             print("Error code: " + str(e))
         print(str(copied) + " files coppied to " + str(network_location))
 
+    # This function drives most of the script. It iterates through the urls and downlaods the data one at at a time.
     def download_all(self, csv_dict):
         global email, school_url, download_file_path, folder, error_count, event_saved_search_name
-        with autohandshake.HandshakeSession(school_url, email, None, 3000,
-                                            None, os.path.abspath(download_file_path)) as browser:
+        with autohandshake.HandshakeSession(school_url, email, None, 3000, "chromedriver.exe",
+                                            os.path.abspath(download_file_path)) as browser:
             csv_dict_reverse = {v: k for k, v in csv_dict.items()}
             for i in tqdm(csv_dict):
                 try:
+                    # Get folder names
                     folder = csv_dict_reverse[csv_dict[i]]
+
+                    # If the URL is an insights page, download with InsightsPage from autohandshake
                     if 'insights_page' in csv_dict.get(i):
                         # Passes reversed URL dictionary at i for folder name
                         filename = folder + datetime.now().strftime("_%Y-%m-%d") + ".csv"
                         insights_page = InsightsPage(csv_dict[i], browser)
+
+                        # Our appointment page has a custom filter. When using custom filters, "All Results" is not
+                        # availbe so instead we use download rows 0-9999
                         if (i == "appointment"):
                             insights_page.download_file(download_dir=download_file_path, file_name=filename,
                                                         file_type=autohandshake.FileType.CSV, limit=9999)
@@ -117,18 +121,20 @@ class ImpactDownloader:
                             insights_page.download_file(download_dir=download_file_path, file_name=filename,
                                                         file_type=autohandshake.FileType.CSV, max_wait_time=9999999)
 
+                    # If the URL is a survey page, download with SurveyPage from autohandshake
                     elif 'surveys' in csv_dict.get(i):
                         survey_id = str(csv_dict.get(i))[-5:]
-                        print("SID: "+survey_id)
+                        print("SID: " + survey_id)
                         survey = SurveyPage(survey_id, browser)
                         survey.download_responses(download_file_path)
                         ImpactDownloader.survey_rename(self)
 
+                    # If the URL is an event page, navigate to the url with browser.get and then use
+                    # EventsPage.downlaod_event_data() to download the file. The EventPage requires using a save search
+                    # and currently has a bug that requires 1 click from the user. This is why I am using a workaround here.
                     elif 'events' in csv_dict.get(i):
                         events_page = EventsPage(browser)
-                        print("\n" + event_saved_search_name)
-                        events_page.load_saved_search(event_saved_search_name)
-                        #browser.click_element_by_xpath("/html/body/div[2]/div[3]/div/div[2]/div[1]/form/div/div[2]/div/div[3]/div/div[1]/ul/li[1]/div/h3/span")
+                        browser.get(csv_dict.get(i))
                         events_page.download_event_data(download_file_path, wait_time=5000)
                         ImpactDownloader.event_rename(self)
 
@@ -138,36 +144,37 @@ class ImpactDownloader:
                 except Exception as e:
                     print("\nError: " + str(e))
                     error_count = error_count + 1
+            # Output the number of erros resulted in data not being downloaded
             print("\nErrors: " + str(error_count))
             return
 
-    #Rename survey files.
+    # Rename survey files.
     def survey_rename(self):
         print("survey rename")
         surveyLocation = os.path.join(download_file_path, 'survey_response_download*.csv')
         surveyLocation = os.path.normpath(surveyLocation)
-        print("SL: "+ surveyLocation)
         files = glob.glob(surveyLocation)
         print(files)
         for file in files:
-            print("Survey File: " + str(file) + "     Folder: " + str(folder))
             dst = os.path.join(download_file_path, folder + datetime.now().strftime("_%Y-%m-%d") + ".csv")
             dst = os.path.normpath(dst)
+            print("Survey Final Name: " + dst)
             os.rename(file, dst)
 
-    #Rename event files.
+    # Rename event files.
     def event_rename(self):
         print("event rename")
         eventLocation = os.path.join(download_file_path, 'event_download*.csv')
         eventLocation = os.path.normpath(eventLocation)
-        print("EL: "+ eventLocation)
         files = glob.glob(eventLocation)
         print(files)
         for file in files:
-            print("Event File: "+str(file)+"     Folder: "+ str(folder))
             dst = os.path.join(download_file_path, folder + datetime.now().strftime("_%Y-%m-%d") + ".csv")
             dst = os.path.normpath(dst)
-            os.rename(file,dst)
+            print("Event Final Name: " + dst)
+
+            os.rename(file, dst)
+
 
 # Create or load config file and assign variables.
 def load_config():
@@ -178,7 +185,7 @@ def load_config():
     if not my_file.is_file():
         file = open("ImpactDownloader.config", "w+")
         file.write(
-            "[DEFAULT]\nEMAIL = \nSCHOOL_URL= \nINPUT_CSV_FILE_PATH = \nNUMBER_OF_ROWS = \nEVENT_SAVED_SEARCH_NAME = \nDOWNLOAD_LOCATION = "
+            "[DEFAULT]\nEMAIL = \nSCHOOL_URL= \nINPUT_CSV_FILE_PATH = \nNUMBER_OF_ROWS = \nDOWNLOAD_LOCATION = "
             "\nNETWORK_LOCATION =  \nLOG_TO_FILE = \nDELETE_AFTER_DAYS = \n")
         messagebox.showinfo("Warning",
                             "Config file created. Please add a CSV file path and relaunch the program.")
@@ -189,14 +196,14 @@ def load_config():
     school_url = config['DEFAULT']['SCHOOL_URL']
     input_file_path = config['DEFAULT']['INPUT_CSV_FILE_PATH']
     number_of_rows = int(config['DEFAULT']['NUMBER_OF_ROWS'])
-    event_saved_search_name = str(config['DEFAULT']['EVENT_SAVED_SEARCH_NAME'])
     download_file_path = config['DEFAULT']['DOWNLOAD_LOCATION']
     network_location = config['DEFAULT']['NETWORK_LOCATION']
     log_enabled = config['DEFAULT']['LOG_TO_FILE']
-    days_until_delete = config['DEFAULT']['DELETE_AFTER_DAYS']
+    days_until_delete = int(config['DEFAULT']['DELETE_AFTER_DAYS'])
     # Quit if there is no CSV path set in the config file
     if input_file_path is "":
-        messagebox.showinfo("Warning", "Please enter an input CSV file path into the config file and relaunch the program.")
+        messagebox.showinfo("Warning",
+                            "Please enter an input CSV file path into the config file and relaunch the program.")
         sys.exit()
 
 
@@ -214,10 +221,9 @@ def delete_csv_from_download():
         print("Error Deleting Old Files: " + str(e))
 
 
-
 def main(impact_downloader):
     print("Main Method: ")
-    global input_file_path, number_of_rows, download_count, missed_urls, log_to_file, download_file_path
+    global input_file_path, number_of_rows, download_count, missed_urls, log_to_file, download_file_path, days_until_delete
     download_count = 0
     download_file_path = ""
     input_file_path = ""
@@ -227,7 +233,7 @@ def main(impact_downloader):
     csv = impact_downloader.load_csv()
     impact_downloader.download_all(csv)
     impact_downloader.copy_to_network_drive()
-    # impact_downloader.delete_old_network_files()
+    impact_downloader.delete_old_network_files(days_until_delete)
 
 
 error_count = 0
